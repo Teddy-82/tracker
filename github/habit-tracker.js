bb2196a25d4cbfdc914ba9e414e1fc40e0a2fa0c
MatildaBjorken <70638324+MatildaBjorken@users.noreply.github.com>
2020-10-19 14:34:20 +0200

    Add files via upload

---
+++ github/habit-tracker.js
@@ -0,0 +1,184 @@
+//main list
+
+let list = []
+
+
+// target from html
+const addBtn = document.querySelector('.addBtn')
+const habitInput = document.querySelector('.habit-input')
+const habitList = document.querySelector('.habit-list')
+const errorEmpty = document.querySelector('.error')
+const newWeek = document.querySelector('.btn')
+const clearAll = document.querySelector('.clear-all')
+const condition = document.querySelector("#condition")
+
+//class eventName {
+//    constructor(event) {
+//        this.createDiv(event)
+//    }
+//}
+const changeBtn = document.querySelector('.items')
+
+//event listeners
+addBtn.addEventListener('click', addHabit)
+habitList.addEventListener('click', deleteAction)
+changeBtn.addEventListener('click', colorChange)
+clearAll.addEventListener('click', removeAll)
+
+
+
+document.addEventListener('keypress', function(enter) {
+  if (enter.keyCode === 13 || enter.which === 13) {
+    addHabit(event)
+  };
+});
+
+
+//function for changing color of the habits
+
+function colorChange(x) {
+ 
+  
+  const button = x.target
+  
+  if (button.classList[0]=== 'check') {
+    condition.textContent = "nice work!"
+    const color = button
+    color.classList.toggle('c')
+
+  } else {
+    const color = button
+    color.classList.remove('c')
+  }
+}
+
+
+/*
+habitInput.addEventListener("keyup", addHabit) 
+  if (event.keyCode === 13) {
+   event.preventDefault();
+   alert('ehue')
+  }
+  
+*/
+
+function addHabit(event){
+    //prevent form from submitting
+    event.preventDefault()
+
+    //if the field is empty
+    if (habitInput.value === '' ) {
+        errorEmpty.classList.remove('hidden')
+    
+    }else {
+        errorEmpty.classList.add('hidden')
+        //create a habit div
+     
+       
+        const habitDiv = document.createElement('div')
+        habitDiv.classList.add('habits')
+
+        //remove btn
+        const trashHabit = document.createElement('button')
+        trashHabit.innerHTML = '<p class="removeBtn">-</p>'
+        trashHabit.classList.add('trash-btn')
+        habitDiv.appendChild(trashHabit)
+
+        //create li
+        const newHabit = document.createElement('li')
+        newHabit.innerText = habitInput.value
+        newHabit.classList.add('habit-item')
+        habitDiv.appendChild(newHabit)
+
+        //check 
+
+        //edit
+        //const editHabit = document.createElement('button');
+    	  //editHabit.classList.add('edit-btn')
+        //editHabit.innerHTML = '<p class="editBtn">edit</p>'
+        //habitDiv.appendChild(editHabit)
+
+        //checkboxes adding using inner HTML
+
+        const checkHabit = document.createElement('row')
+        checkHabit.innerHTML = 
+        '<div class= "selected"><button class="check"></button><button class="check"></button><button class="check"></button><button class="check"></button><button class="check"></button><button class="check"></button><button class="check"></button></div>'
+
+        checkHabit.classList.add('table-check')
+        
+        habitDiv.appendChild(checkHabit)
+
+
+        //append to list
+
+        habitList.appendChild(habitDiv)
+
+        
+    }  
+    
+    //to clear the input after adding
+    habitInput.value= ''
+    condition.textContent = "there u go!"
+}
+
+
+
+//remove btn
+function deleteAction(e) {
+  const item = e.target
+  if (item.classList[0]=== 'trash-btn'){
+      const habit = item.parentElement
+ 
+      habit.remove()
+  }
+}
+
+//remove all btn
+
+function removeAll() {
+  alert ('remove')
+}
+
+       //set count
+
+
+
+
+//counter
+let counter = document.querySelector('.counter');
+const addCount = document.querySelector('#addCountBtn');
+const lowerCount = document.querySelector('#lowerCountBtn');
+
+
+let count = 0;
+
+
+addCount.addEventListener('click', incrementCounter);
+lowerCount.addEventListener('click', decrementCounter);
+
+function incrementCounter (){
+    count++;
+    counter.innerHTML =  count;
+    
+}
+
+function incrementCounter (){
+    count++;
+    counter.innerHTML =  count;
+    if(counter.innerHTML>'1'){
+        counter.style.color = 'white'
+    }
+    
+    counter.animate([{opacity:'0.2'},{opacity:'1.0'}], {duration: 1000, fill:'forwards'})
+}
+;
+
+function decrementCounter (){
+    count--;
+    counter.innerHTML =  count;
+    if(counter.innerHTML<'1'){
+        count++
+    }
+  
+    counter.animate([{opacity:'0.2'},{opacity:'1.0'}], {duration: 1000, fill:'forwards'})
+}
